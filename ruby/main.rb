10cc433dfbd2990ab8cb09219b951ee215d9f8eb
Alec Adub <adub.alec@gmail.com>
2020-12-06 17:25:41 -0500

    q3 done

--- ruby/main.rb
+++ ruby/main.rb
@@ -16,34 +16,52 @@ r.print
 
 puts 'Q2 & Q3:'
 
+stats = {
+  'Shape': 0,
+  'Circle': 0,
+  'Rectangle': 0,
+  'Ellipse': 0
+}
+
 File.readlines('./input.txt').each do |line|
   line_array = line.split(' ')
   if line_array[0] == 'shape'
     s = Shape.new
     s.print
+    stats[:Shape] = stats[:Shape] + 1
   end
   if line_array[0] == 'circle'
-    if line_array[1]&.to_i&.positive?
+    if line_array[1].to_i >= 0
       c = Circle.new(line_array[1].to_i)
       c.print
+      stats[:Shape] = stats[:Shape] + 1
+      stats[:Circle] = stats[:Circle] + 1
     else
       puts 'Error: Invalid circle'
     end
   end
   if line_array[0] == 'rectangle'
-    if line_array[1]&.to_i&.positive? && line_array[2]&.to_i&.positive?
+    if line_array[1].to_i >= 0 && line_array[2].to_i >= 0
       r = Rectangle.new(line_array[1].to_i, line_array[2].to_i)
       r.print
+      stats[:Shape] = stats[:Shape] + 1
+      stats[:Rectangle] = stats[:Rectangle] + 1
     else
       puts 'Error: Invalid rectangle'
     end
   end
   if line_array[0] == 'ellipse'
-    if line_array[1]&.to_i&.positive? && line_array[2]&.to_i&.positive?
+    if line_array[1].to_i >= 0 && line_array[2].to_i >= 0
       e = Ellipse.new(line_array[1].to_i, line_array[2].to_i)
       e.print
+      stats[:Shape] = stats[:Shape] + 1
+      stats[:Circle] = stats[:Ellipse] + 1
     else
       puts 'Error: Invalid ellipse'
     end
   end
+end
+
+stats.each do |key, value|
+  puts "#{key} : #{value}"
 end
